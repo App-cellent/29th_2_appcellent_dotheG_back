9c3d6c540b5f9cd97698e29f7c88798248527836
youngseo22 <ysnoh22@sookmyung.ac.kr>
2025-01-06 15:09:47 +0900

    style(quiz) : 퀴즈 컨트롤러 reponse 정리

--- dotheG/src/main/java/com/example/dotheG/controller/QuizController.java
+++ dotheG/src/main/java/com/example/dotheG/controller/QuizController.java
@@ -2,6 +2,7 @@ package com.example.dotheG.controller;
 
 import com.example.dotheG.dto.QuizDto;
 import com.example.dotheG.dto.QuizRequestDto;
+import com.example.dotheG.dto.Response;
 import com.example.dotheG.service.QuizService;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,13 +18,15 @@ public class QuizController {
 
     // 오늘의 퀴즈 이미 풀었는지 조회
     @GetMapping()
-    public void check(@RequestBody QuizRequestDto quizRequestDto){
+    public Response<Object> check(@RequestBody QuizRequestDto quizRequestDto){
         quizService.check(quizRequestDto.getUserId(), quizRequestDto.getQuizId());
+        return Response.success("퀴즈 조회", quizService.check(quizRequestDto.getUserId(), quizRequestDto.getQuizId()));
     }
 
     // 퀴즈 풀기
     @PostMapping("/solve")
-    public void solve(@RequestBody QuizDto quizDto){
+    public Response<Object> solve(@RequestBody QuizDto quizDto){
         quizService.solve(quizDto.getUserId(), quizDto.getQuizId(), quizDto.getMyAnswer());
+        return Response.success("걸음수 업데이트", null);
     }
 }
