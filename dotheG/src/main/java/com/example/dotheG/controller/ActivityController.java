810c0227088c2781dcafb67f5c8902376c95bdad
youngseo22 <ysnoh22@sookmyung.ac.kr>
2025-01-06 15:27:56 +0900

    style(activity) : 컨트롤러 reponse 수정

--- dotheG/src/main/java/com/example/dotheG/controller/ActivityController.java
+++ dotheG/src/main/java/com/example/dotheG/controller/ActivityController.java
@@ -2,6 +2,7 @@ package com.example.dotheG.controller;
 
 import com.example.dotheG.dto.ActivityResponseDto;
 import com.example.dotheG.dto.MemberActivityDto;
+import com.example.dotheG.dto.Response;
 import com.example.dotheG.service.ActivityService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/upload")
@@ -22,38 +24,38 @@ public class ActivityController {
 
     // OpenCV 사용 (사진 분석)
     @GetMapping("/analyze")
-    public ResponseEntity<Long> analyzePhoto(@RequestPart("activityImage") MultipartFile activityImage){
+    public Response<Objects> analyzePhoto(@RequestPart("activityImage") MultipartFile activityImage){
         try {
             Long analyzedActivityId = activityService.analyzePhoto(activityImage);
-            return ResponseEntity.ok(analyzedActivityId);
+            return Response.success("이미지 분석", null);
         } catch (Exception e){
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+            return Response.success("이미지 분석 실패" + HttpStatus.INTERNAL_SERVER_ERROR, null);
         }
     }
 
     // 퀘스트 등록하기 (사진 결과 저장)
     // [이미지, 활동 명, 날짜] 저장되어야 함 + 리워드 지급 받아야 함
     @PostMapping("/certification")
-    public ResponseEntity<String> requestImage(
+    public Response<Objects> requestImage(
             @RequestPart(value = "file", required = false) MultipartFile activityImage,
             @RequestParam("userInfoId") Long userInfoId) {
         try {
             activityService.requestImage(activityImage, userInfoId);
-            return ResponseEntity.ok("Certification successfully registered.");
+            return Response.success("퀘스트 등록", null);
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body("Error: " + e.getMessage());
+            return Response.success("퀘스트 등록 실패" + e.getMessage(), null);
         }
     }
 
     // 오늘의 인증 조회하기
     @GetMapping("/today")
-    public ResponseEntity<List<ActivityResponseDto>> viewToday(@RequestBody MemberActivityDto memberActivityDto) {
+    public Response<Objects> viewToday(@RequestBody MemberActivityDto memberActivityDto) {
         List<ActivityResponseDto> activities = activityService.viewToday(memberActivityDto.getUserId());
 
         if (activities.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            return Response.success("오늘 인증 기록 없음", null);
         }
-        return ResponseEntity.ok(activities);
+        return Response.success("오늘 인증 기록 조회", null);
     }
 
 }
