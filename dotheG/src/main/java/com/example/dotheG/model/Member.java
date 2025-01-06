f5da5915ac84c05271f8bac624057912b890e6ba
youngseo22 <ysnoh22@sookmyung.ac.kr>
2025-01-06 13:25:48 +0900

    first message

---
+++ dotheG/src/main/java/com/example/dotheG/model/Member.java
@@ -0,0 +1,40 @@
+package com.example.dotheG.model;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "USER_ID")
+    private Long userId;
+
+    private String userName;
+
+    private String userLogin;
+
+    private String userPassword;
+
+    private boolean available;
+
+    @Column(columnDefinition = "boolean default false")
+    private boolean isSocial;
+
+    private String role;
+
+    @Builder
+    public Member(String userName,String userLogin ,String userPassword, boolean available, boolean isSocial, String role) {
+        this.userName = userName;
+        this.userLogin = userLogin;
+        this.userPassword = userPassword;
+        this.available = available;
+        this.isSocial = isSocial;
+        this.role = role;
+    }
+}
---
+++ dotheG/src/main/java/com/example/dotheG/model/MemberInfo.java
@@ -0,0 +1,21 @@
+package com.example.dotheG.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class MemberInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "USER_INFO_ID")
+    private Long userInfoId;
+
+    @OneToOne
+    @JoinColumn(name = "USER_ID")
+    private Member userId;
+
+    // Todo : default value = 20
+    @Column(columnDefinition = "integer default 20")
+    private int userReward;
+
+    private Long mainChar;
+}
