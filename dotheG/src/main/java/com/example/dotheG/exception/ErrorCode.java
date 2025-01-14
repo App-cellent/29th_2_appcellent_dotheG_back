115314d61b55678822e80ab7a6c5eff379f33428
EunSeo0117 <153796118+EunSeo0117@users.noreply.github.com>
2025-01-15 01:39:56 +0900

    style(ErrorCode): 오타수정

--- dotheG/src/main/java/com/example/dotheG/exception/ErrorCode.java
+++ dotheG/src/main/java/com/example/dotheG/exception/ErrorCode.java
@@ -25,7 +25,7 @@ public enum ErrorCode {
     REWARD_ALREADY_GRANTED(HttpStatus.BAD_REQUEST, "이미 리워드가 지급되었습니다."),
     ACTIVITY_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 활동이 등록되어있지 않습니다."),
     MYACTIVITY_NOT_FOUND(HttpStatus.NOT_FOUND, "오늘 인증한 활동이 없습니다."),
-    INVALID_INPUT(HttpStatus.BAD_REQUEST, "유효하지 않은 객체입니다.");
+    INVALID_INPUT(HttpStatus.BAD_REQUEST, "유효하지 않은 객체입니다."),
     NAME_NOT_WRITTEN(HttpStatus.BAD_REQUEST, "닉네임을 입력해 주세요."),
     NAME_FAILED(HttpStatus.UNPROCESSABLE_ENTITY, "길이에 맞는 닉네임을 입력해주세요."),
     PASSWORD_DIFFERENT(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
