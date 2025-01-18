fdd4a8093c4e8156a24ba2f91a786ceb2ea5d234
youngseo22 <ysnoh22@sookmyung.ac.kr>
2025-01-19 02:31:01 +0900

    feat(mainpage) : 홈 화면 get 기능 구현

--- dotheG/src/main/java/com/example/dotheG/repository/ActivityRepository.java
+++ dotheG/src/main/java/com/example/dotheG/repository/ActivityRepository.java
@@ -2,8 +2,20 @@ package com.example.dotheG.repository;
 
 import com.example.dotheG.model.Activity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
+
+    // 오늘의 퀘스트
+    @Query(value = """
+        SELECT activity
+        FROM Activity activity
+        WHERE activity.activityId BETWEEN :startId AND :endId
+        ORDER BY RAND()
+        LIMIT 1
+    """)
+    Activity findRandomActivity(@Param("startId") int startId, @Param("endId") int endId);
 }
