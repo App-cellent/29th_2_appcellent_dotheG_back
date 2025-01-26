36dc83b179c05c2bada0e98d1f410a2fc2705b69
haejee0514 <haejee0514@naver.com>
2025-01-26 17:34:11 +0900

    fix(weekreport): "week_report 쿼리문 수정"

--- dotheG/src/main/java/com/example/dotheG/repository/WeekReportRepository.java
+++ dotheG/src/main/java/com/example/dotheG/repository/WeekReportRepository.java
@@ -10,13 +10,13 @@ import java.time.LocalDate;
 import java.util.Optional;
 
 public interface WeekReportRepository extends JpaRepository<WeekReport, Long> {
-    @Query("SELECT COUNT(w) > 0 FROM WeekReport w WHERE w.userId = :user AND w.weekStartDate = :startDate AND w.weekEndDate = :endDate")
+    @Query("SELECT COUNT(w) > 0 FROM week_report w WHERE w.userId = :user AND w.weekStartDate = :startDate AND w.weekEndDate = :endDate")
     boolean existsByUserAndWeekRange(
             @Param("user") Member user,
             @Param("startDate") LocalDate startDate,
             @Param("endDate") LocalDate endDate
     );
 
-    @Query(value = "SELECT * FROM WEEK_REPORT WHERE USER_ID = :userId ORDER BY WEEK_REPORT_ID DESC LIMIT 1", nativeQuery = true)
+    @Query(value = "SELECT * FROM week_report WHERE USER_ID = :userId ORDER BY WEEK_REPORT_ID DESC LIMIT 1", nativeQuery = true)
     Optional<WeekReport> findLatestReportByUser(@Param("userId") Long userId);
 }
