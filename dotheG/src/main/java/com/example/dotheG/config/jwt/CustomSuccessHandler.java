d9bda2e0b9c1c165600c24012750ac3425a60702
hyunchin2 <hyun0325@sookmyung.ac.kr>
2025-02-01 18:22:04 +0900

    Fix: 네이버 로그인 redirect uri 수정 , 회우너가입시 추가 테이블 생성

--- dotheG/src/main/java/com/example/dotheG/config/jwt/CustomSuccessHandler.java
+++ dotheG/src/main/java/com/example/dotheG/config/jwt/CustomSuccessHandler.java
@@ -34,7 +34,7 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         String token = jwtUtil.createToken("authorization", username, role, 600000*36L);
         //쿠키 방식으로 토큰 생성
         response.addCookie(createCookie("authorization", token));
-        response.sendRedirect("http://localhost:8081/mainpage/getInfo");
+        response.sendRedirect("dotheg://oauth/callback");
     }
     //쿠키 생성 메소드
     private Cookie createCookie(String key, String value) {
