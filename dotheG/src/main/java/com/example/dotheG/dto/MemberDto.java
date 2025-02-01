196a67e5557a1c6283a2c087bcee275aa9a6c2a4
Lee Hyun Chin <140608054+hgeniee@users.noreply.github.com>
2025-02-01 13:40:30 +0900

    Revert "Feat: 네이버 소셜 로그인 구현"

--- dotheG/src/main/java/com/example/dotheG/dto/MemberDto.java
+++ dotheG/src/main/java/com/example/dotheG/dto/MemberDto.java
@@ -1,30 +1,28 @@
 package com.example.dotheG.dto;
 
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class MemberDto {
 
-    private String userName;
+    private final String userName;
 
-    private String userLogin;
+    private final String userLogin;
 
     private String userPassword;
 
-    private boolean available;
+    private final boolean available;
 
-    private boolean isSocial;
+    private final boolean isSocial;
 
-    private String role = "USER";
+    private final String role;
 
-    @Builder
-    public MemberDto(String userLogin, String userName, String userPassword, boolean available, boolean isSocial, String role) {
+    public MemberDto(String userLogin, String userName, String userPassword) {
         this.userName = userName;
         this.userLogin = userLogin;
         this.userPassword = userPassword;
-        this.available = available;
-        this.isSocial = isSocial;
-        this.role = role;
+        this.available = true;
+        this.isSocial = false;
+        this.role = "USER";
     }
 }
