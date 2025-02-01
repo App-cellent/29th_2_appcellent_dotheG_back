5daeb03f3e79292f4ba1d26c86b1cc9c35455c47
hyunchin2 <hyun0325@sookmyung.ac.kr>
2025-02-01 14:44:48 +0900

    Fix: 네이버 소셜 로그인 에러 수정

--- dotheG/src/main/java/com/example/dotheG/config/jwt/JwtFilter.java
+++ dotheG/src/main/java/com/example/dotheG/config/jwt/JwtFilter.java
@@ -1,14 +1,18 @@
 package com.example.dotheG.config.jwt;
 
 import com.example.dotheG.dto.CustomUserDetails;
+import com.example.dotheG.dto.MemberDto;
+import com.example.dotheG.dto.oAuth2.CustomOAuth2User;
 import com.example.dotheG.exception.CustomException;
 import com.example.dotheG.exception.ErrorCode;
 import com.example.dotheG.model.Member;
 import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,6 +21,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@Slf4j
 public class JwtFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
@@ -27,8 +32,21 @@ public class JwtFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.info("[Jwt Filter] 요청 확인");
 
         String accessToken = request.getHeader("access");
+
+        log.info("[JwtFilter] Access token: {}", accessToken);
+        String authorization = null;
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                log.info("[JwtFilter] Cookie : {} 확인", cookie);
+                if (cookie.getName().equals("authorization")) {
+                    authorization = cookie.getValue();
+                }
+            }
+        }
 
         if (accessToken == null) {
             filterChain.doFilter(request, response);
@@ -68,6 +86,27 @@ public class JwtFilter extends OncePerRequestFilter {
 
         Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
         SecurityContextHolder.getContext().setAuthentication(authToken);
+
+        if (authorization == null) {
+            System.out.println("token null");
+            filterChain.doFilter(request, response);
+            return;
+        }
+        String token = authorization;
+        if (jwtUtil.isExpired(token)) {
+            System.out.println("token expired");
+            filterChain.doFilter(request, response);
+            return;
+        }
+        String userSocialLogin = jwtUtil.getUsername(token);
+        String socialRole = jwtUtil.getRole(token);
+        MemberDto socialMember = MemberDto.builder()
+                .userLogin(userSocialLogin)
+                .role(socialRole)
+                .build();
+        CustomOAuth2User customOAuth2User = new CustomOAuth2User(socialMember);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(customOAuth2User, null, customOAuth2User.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
 
         filterChain.doFilter(request, response);
     }
